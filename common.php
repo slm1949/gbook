8e98f38d2dbb5a3b1b384dc18a54b7d88bf4037c
slm1949 <slm1949@163.com>
2012-09-28 00:55:06 +0800

    改admin_pass为admin_password

--- common.php
+++ common.php
@@ -6,7 +6,7 @@ $dataname="gbook";
 $t_name="gbook";
 $p_num="10";                                     //每页显示的条数
 $admin_name="admin";                             //管理员用户名
-$admin_pass="12345";                             //管理员密码
+$admin_password="12345";                             //管理员密码
 $my_connect=mysql_connect($host,$user,$pass);    //连接数据库
 mysql_select_db($dataname,$my_connect);          //选择数据库
 ?>
