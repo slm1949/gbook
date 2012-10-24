53aacc98b19bb5ff469fafe865b5ccfe86c2e73a
slm1949 <slm1949@163.com>
2012-10-24 22:05:10 +0800

    初步INDEX单一入口文件

--- index.php
+++ index.php
@@ -1,9 +1,22 @@
 <?php
 // 项目入口
-// 根据不通的参数调用不通的controller
+// 根据不同的参数调用不同的controller
 // controller=?&action=?
 // 定义常用的常量
+define('DS', DIRECTORY_SEPARATOR);
 define('WEBROOT', dirname(__FILE__));
 define('VIEW', WEBROOT.DS.'view');
 define('MODEL', WEBROOT.DS.'model');
 define('CONTROLLER', WEBROOT.DS.'controller');
+ switch($_GET[controller])
+{
+  case user_controller:
+      include CONTROLLER.DS.'user_controller.php';
+  break;
+  case admin_controller:
+       include CONTROLLER.DS.'admin_controller.php';
+  break;
+  default:
+  echo '取得CONTRLE方法错误';
+ }
+
