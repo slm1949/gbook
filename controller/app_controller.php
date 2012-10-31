973ebb821a419d26bba75613912100ad4e9ab3e6
slm1949 <slm1949@163.com>
2012-10-31 23:09:28 +0800

    初次写CONTROLLER代码

--- controller/app_controller.php
+++ controller/app_controller.php
@@ -1,8 +1,14 @@
 <?php
 // 对web的操作，例如处理文件上传，数据过滤，数据验证，数据转化
-class AppController{
-	// 渲染view模版
-	public function render($view){
-
+class AppController{              //控制器将$_GET['action']中不同的参数（add、modify、delete）
+	var $model;
+        var $view;
+        public function __construct(&$dao){  //地址传值
+          $this->model=& new basemodel($dao);
+        }
+        public function getview(){
+         return $this->view;
+	public function render($view){    //这个函数用来做什么呢？
+          
 	}
 }
--- controller/index_controller.php
+++ controller/index_controller.php
@@ -4,3 +4,23 @@ class IndexController extends AppController{
 		render('index');
 	}
 }
+
+class ShowController extents AppController{   //控制显示留言的子类
+      public function __construct(&$dao){
+         parent::__construct($dao);
+         $this->view=& new ShowView($this->model); //把model对象传给View子类供其获取数据   
+       }
+}
+class AddController extents Controller{       //用于控制添加留言的子类
+      public function __construct(&$dao,$add){
+      parent::__construct($dao);
+      $this->view=& new AddView($this->model,$add);    //$add的实参为$_POST数组
+                        //表单中的留言项目存储在该系统数组中
+class DeleteController extends Appcontroller{   //用于控制删除留言的子类
+  function __construct(&$dao,$id){
+  parent::__construct($dao);
+  $this->view=& new DeleteView($this->model,$id);
+  }
+}
+?>  
+
