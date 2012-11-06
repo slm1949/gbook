66b2c7d1f68c980a74dabc8b83d95c344ce0225d
root <root@songlm-K50IN.(none)>
2012-11-06 23:27:03 +0800

    程序调试，修改bug

--- index.php
+++ index.php
@@ -8,15 +8,35 @@ define('WEBROOT', dirname(__FILE__));
 define('VIEW', WEBROOT.DS.'view');
 define('MODEL', WEBROOT.DS.'model');
 define('CONTROLLER', WEBROOT.DS.'controller');
- switch($_GET[controller])
+include WEBROOT.DS.'config'.DS.'db.config.php';
+include MODEL.DS.'base_model.php';
+include MODEL.DS.'note_model.php';
+include CONTROLLER.DS.'app_controller.php';
+include CONTROLLER.DS.'index_controller.php';
+//include VIEW.DS.'common'.DS.'header.php';//不应该写在这里
+//include VIEW.DS.'common'.DS.'footer.php';//不应该写在这里
+include VIEW.DS.'index'.DS.'index.php';
+
+$dao=new DataAccess($host,$user,$pass,$dataname,$t_name);//建立数据库连接对象
+$model=new NoteModel($dao);  //建立了一个数据处理的对象
+ switch($_GET['action'])
 {
-  case user_controller:
-      include CONTROLLER.DS.'user_controller.php';
+  case 'show':
+  $controller=new ShowController($model);  
+  
   break;
-  case admin_controller:
-       include CONTROLLER.DS.'admin_controller.php';
+  case 'add':
+  $controller=new AddController($dao);  
+  break;
+  case 'delete':
+  $controller=new DelecteController($dao);
   break;
   default:
-  echo '取得CONTRLE方法错误';
+  echo 'case defalt 被执行';
+  $controller=new ShowController($model);//$model是NoteMode类型 
+     
  }
+$view=$controller->getView(); //获取视图对象
+echo '获取视图正确';
+$view->display();             //输出HTML
 
